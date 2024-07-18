fbb149133af26307f08b02e1365559cde8f65ede
Altair Sossai <altairsossai@gmail.com>
2024-07-18 09:02:24 -0300

    Optimize web app and enhance dev workflow
    - Integrated WebMarkupMin into the L4D2PlayStats.Web project to minify and compress HTML, CSS, and JavaScript files, improving performance.
    - Configured WebMarkupMin in the startup to enable minification and compression in the development environment, including HTML minification and HTTP response compression setup.
    - Enabled Razor runtime compilation in Program.cs, allowing for immediate updates to views without needing to restart the application, enhancing the developer experience.
    - Added WebMarkupMin middleware to apply minification and compression settings to web content served to clients.

--- L4D2PlayStats.Web/L4D2PlayStats.Web/Program.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Program.cs
@@ -1,10 +1,20 @@
 using L4D2PlayStats.DependencyInjection;
 using L4D2PlayStats.Sdk.DependencyInjection;
+using WebMarkupMin.AspNetCore8;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .AddJsonFile("appsettings.json", false, true);
+
+builder.Services
+    .AddWebMarkupMin(options =>
+    {
+        options.AllowMinificationInDevelopmentEnvironment = true;
+        options.AllowCompressionInDevelopmentEnvironment = true;
+    })
+    .AddHtmlMinification()
+    .AddHttpCompression();
 
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddApp();
@@ -15,6 +25,7 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+app.UseWebMarkupMin();
 app.MapControllers();
 app.MapControllerRoute("default", "{controller=Ranking}/{action=Index}/{id?}");
 app.Run();
