55bc8cdabee0a7087fab716425a89cd15beae010
Altair Sossai <altairsossai@gmail.com>
2024-07-23 11:42:25 -0300

    Deploy L4D2PlayStats.Web via script
    This commit introduces a script that automates the deployment of the L4D2PlayStats.Web application. Key steps include terminating any existing screen session named "l4d2-playstats-web", creating the necessary directory structure, extracting the application files from `publish.tar.gz` into the target directory, setting the executable permission for the main application file, and initiating a new detached screen session to run the application. Additionally, the script cleans up by deleting the `publish.tar.gz` file after extraction and setup.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,6 @@ if screen -list | grep -q "l4d2-playstats-web"; then
   screen -S l4d2-playstats-web -X quit
 fi
 
-mkdir -p /home/l4d2-playstats-web
 tar -xzf /home/devops/publish.tar.gz -C /home/l4d2-playstats-web
 chmod +x /home/l4d2-playstats-web/L4D2PlayStats.Web
 screen -d -m -S "l4d2-playstats-web" sh -c 'cd /home/l4d2-playstats-web && ./L4D2PlayStats.Web'
