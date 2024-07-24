740a155feb35a397cb67efaf0ec6f997318514c4
Altair Sossai <altairsossai@gmail.com>
2024-07-24 16:27:22 -0300

    Add DaysRange property and update Index.cshtml
    Added a new property `DaysRange` to the `ExperienceConfigResult` class in `ExperienceConfigResult.cs`. Updated `Index.cshtml` to dynamically display the number of days from the `DaysRange` property of the `Config` model instead of the hardcoded value of 45 days, enhancing flexibility and configurability.

--- L4D2PlayStats.Web/L4D2PlayStats.Sdk/Ranking/Results/ExperienceConfigResult.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Sdk/Ranking/Results/ExperienceConfigResult.cs
@@ -2,6 +2,7 @@
 
 public class ExperienceConfigResult
 {
+    public int DaysRange { get; set; }
     public int Win { get; set; }
     public int Loss { get; set; }
     public int Mvps { get; set; }
