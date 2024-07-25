181d4230621b07a505c3661f104fde8500eab66a
Altair Sossai <altairsossai@gmail.com>
2024-07-25 09:01:40 -0300

    Add PlayersController and PlayerPatentModel classes
    Introduced PlayersController with API route and methods to handle
    player data retrieval and processing. Added PlayerPatentModel to
    represent player patent data. Minor formatting fix in _Match.cshtml.

---
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Controllers/Api/PlayersController.cs
@@ -0,0 +1,27 @@
+﻿using L4D2PlayStats.Patents.Services;
+using L4D2PlayStats.Ranking;
+using L4D2PlayStats.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L4D2PlayStats.Web.Controllers.Api;
+
+[Route("api/players")]
+[ApiController]
+public class PlayersController(IRankingServiceCached rankingService, IPatentService patentService) : ControllerBase
+{
+    [HttpGet("patents")]
+    public async Task<IActionResult> Patents()
+    {
+        var players = await rankingService.GetAsync();
+
+        var models = players.Select(player =>
+        {
+            var patentProgress = patentService.GetPatentProgress(player);
+            var model = new PlayerPatentModel(player, patentProgress);
+
+            return model;
+        }).ToList();
+
+        return Ok(models);
+    }
+}
---
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Models/PlayerPatentModel.cs
@@ -0,0 +1,10 @@
+﻿using L4D2PlayStats.Patents;
+using L4D2PlayStats.Sdk.Ranking.Results;
+
+namespace L4D2PlayStats.Web.Models;
+
+public class PlayerPatentModel(PlayerResult player, PatentProgress patentProgress)
+{
+    public string CommunityId => player.CommunityId.ToString();
+    public int Level => patentProgress.Patent.Level;
+}
