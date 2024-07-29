a11bf1a9b15755cb835015a9c19a178e04fa646b
Altair Sossai <altairsossai@gmail.com>
2024-07-29 17:23:22 -0300

    Refactor server info fetching and update null checks
    Refactored ServersInfoController to include a new private method `GetServerAsync` for fetching server information and players, with added error handling and logging. Updated `ServerModel` to allow nullable `ServerInfo` and `Players` properties. Modified `Index.cshtml` to include null checks and conditional rendering for server information and player details.

--- L4D2PlayStats.Web/L4D2PlayStats.Web/Controllers/ServersInfoController.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Controllers/ServersInfoController.cs
@@ -1,4 +1,6 @@
+using L4D2PlayStats.A2S.Players;
 using L4D2PlayStats.A2S.Players.Services;
+using L4D2PlayStats.A2S.Servers;
 using L4D2PlayStats.A2S.Servers.Services;
 using L4D2PlayStats.Web.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -27,26 +29,47 @@ public class ServersInfoController(
             var servers = new List<ServerModel>();
 
             foreach (var serverIp in ServerIPs)
-            {
-                var segments = serverIp.Split(':');
-
-                if (segments.Length != 2)
-                    throw new ArgumentException("Invalid server IP format");
-
-                if (!int.TryParse(segments[1], out var port))
-                    throw new ArgumentException("Invalid server port");
-
-                var ip = segments[0];
-                var serverInfo = await serverService.GetServerInfoAsync(ip, port);
-                var players = await playerService.GetPlayersAsync(ip, port).ToListAsync();
-                var server = new ServerModel(serverIp, serverInfo, players);
-
-                servers.Add(server);
-            }
+                servers.Add(await GetServerAsync(serverIp));
 
             return servers;
         }))!;
 
         return View(models);
+    }
+
+    private async Task<ServerModel> GetServerAsync(string serverIp)
+    {
+        var segments = serverIp.Split(':');
+
+        if (segments.Length != 2)
+            throw new ArgumentException("Invalid server IP format");
+
+        if (!int.TryParse(segments[1], out var port))
+            throw new ArgumentException("Invalid server port");
+
+        var ip = segments[0];
+
+        ServerInfo? serverInfo = null;
+        List<Player>? players = null;
+
+        try
+        {
+            serverInfo = await serverService.GetServerInfoAsync(ip, port);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+
+        try
+        {
+            players = await playerService.GetPlayersAsync(ip, port).ToListAsync();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+
+        return new ServerModel(serverIp, serverInfo, players);
     }
 }
--- L4D2PlayStats.Web/L4D2PlayStats.Web/Models/ServerModel.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Models/ServerModel.cs
@@ -3,9 +3,9 @@ using L4D2PlayStats.A2S.Servers;
 
 namespace L4D2PlayStats.Web.Models;
 
-public class ServerModel(string serverIp, ServerInfo serverInfo, List<Player> players)
+public class ServerModel(string serverIp, ServerInfo? serverInfo, List<Player>? players)
 {
     public string ServerIp { get; } = serverIp;
-    public ServerInfo ServerInfo { get; } = serverInfo;
-    public List<Player> Players { get; } = players;
+    public ServerInfo? ServerInfo { get; } = serverInfo;
+    public List<Player>? Players { get; } = players;
 }
