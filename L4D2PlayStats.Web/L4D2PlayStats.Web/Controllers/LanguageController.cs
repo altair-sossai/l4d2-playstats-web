491ac22a613a4e2117d701e5eb67bf4c42f7500a
Altair Sossai <altairsossai@gmail.com>
2024-08-06 16:57:23 -0300

    Add localization support and language switcher
    - Added `Microsoft.Extensions.Localization` package for localization.
    - Configured localization services in `Program.cs` for `en-us` and `pt-br`.
    - Localized MVC controllers, views, and data annotations.
    - Introduced `LanguageController` for handling language changes.
    - Updated `Index.cshtml` and `_Layout.cshtml` to use localized strings.
    - Created resource files (`.resx`) for `Index` and `_Layout` views.
    - Added CSS and JavaScript for language switcher in `_Layout.cshtml`.
    - Added `br.png` and `us.png` for language switcher icons.

---
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Controllers/LanguageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L4D2PlayStats.Web.Controllers;
+
+public class LanguageController : Controller
+{
+    [HttpPost]
+    public IActionResult Change(string culture)
+    {
+        Response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+            new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+        );
+
+        return Ok();
+    }
+}
