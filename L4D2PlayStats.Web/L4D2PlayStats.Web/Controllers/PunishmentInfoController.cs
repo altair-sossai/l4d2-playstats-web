c1f77ada4fa778bad781b016df476cced01830bc
João Pedro <joaopedrobdmgbr@gmail.com>
2024-08-06 13:27:39 -0300

    Add punishment info

---
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Controllers/PunishmentInfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L4D2PlayStats.Web.Controllers
+{
+    public class PunishmentInfoController : Controller
+    {
+        // GET: PunishmentInfoController
+        public ActionResult Index()
+        {
+            ViewBag.PunishmentInfo = "active";
+
+            return View();
+        }
+    }
+}
