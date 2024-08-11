0bbbab355cdcc5eacfed3df992a4d37ed1b33c94
Altair Sossai <altairsossai@gmail.com>
2024-08-11 16:01:25 -0300

    Remove WebMarkupMin and authorization middleware
    The `L4D2PlayStats.Web.csproj` file has been modified to remove the `WebMarkupMin.AspNetCore8` package reference. In `Program.cs`, the `WebMarkupMin.AspNetCore8` namespace import has been removed, along with the code that configures and adds the WebMarkupMin services (`AddWebMarkupMin`, `AddHtmlMinification`, and `AddHttpCompression`) to the service collection. The `app.UseWebMarkupMin()` middleware call has been removed from the request pipeline configuration. Additionally, the `app.UseAuthorization()` middleware call has also been removed from the request pipeline configuration.

--- L4D2PlayStats.Web/L4D2PlayStats.Web/Program.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Program.cs
@@ -2,21 +2,11 @@ using System.Globalization;
 using L4D2PlayStats.DependencyInjection;
 using L4D2PlayStats.Sdk.DependencyInjection;
 using Microsoft.AspNetCore.Localization;
-using WebMarkupMin.AspNetCore8;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .AddJsonFile("appsettings.json", false, true);
-
-builder.Services
-    .AddWebMarkupMin(options =>
-    {
-        options.AllowMinificationInDevelopmentEnvironment = true;
-        options.AllowCompressionInDevelopmentEnvironment = true;
-    })
-    .AddHtmlMinification()
-    .AddHttpCompression();
 
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
@@ -46,8 +36,6 @@ app.UseRequestLocalization(new RequestLocalizationOptions
 
 app.UseStaticFiles();
 app.UseRouting();
-app.UseAuthorization();
-app.UseWebMarkupMin();
 app.MapControllers();
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
