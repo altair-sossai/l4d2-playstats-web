b7a231a57f3574d2e17985a8cb3436f11689f21c
Altair Sossai <altairsossai@gmail.com>
2024-08-30 14:17:47 -0300

    Remove OpenAI integration and related dependencies
    The following changes have been made to remove the OpenAI integration from the project:
    
    - Deleted `OpenAiController.cs` which included the `OpenAiController` class and its `LastMatch` method.
    - Updated `L4D2PlayStats.csproj` to remove the `OpenAI` package dependency (version `2.0.0-beta.8`).
    - Deleted `IOpenAiService.cs` which contained the `IOpenAiService` interface.
    - Deleted `PlayerInput.cs` which contained the `PlayerInput` class.
    - Deleted `OpenAiService.cs` which contained the `OpenAiService` class.
    - Deleted `ChatResult.cs` which contained the `ChatResult` class.
    
    These changes completely remove the OpenAI functionality from the project.

--- L4D2PlayStats.Web/L4D2PlayStats/OpenAi/IOpenAiService.cs
+++
@@ -1,9 +0,0 @@
-﻿using L4D2PlayStats.OpenAi.Results;
-using L4D2PlayStats.Sdk.Matches.Results;
-
-namespace L4D2PlayStats.OpenAi;
-
-public interface IOpenAiService
-{
-    Task<ChatResult> LastMatchSummaryAsync(MatchResult match);
-}
