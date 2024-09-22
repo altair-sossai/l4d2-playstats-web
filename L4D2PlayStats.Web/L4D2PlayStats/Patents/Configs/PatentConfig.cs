2760572860439fd42863d479301c990a530ff659
Altair Sossai <altairsossai@gmail.com>
2024-09-22 12:48:39 -0300

    Increase MaxExperience in PatentConfig to 3000
    The MaxExperience constant in the PatentConfig class has been
    increased from 2,500 to 3,000. This change accommodates higher
    experience levels within the application.

--- L4D2PlayStats.Web/L4D2PlayStats/Patents/Configs/PatentConfig.cs
+++ L4D2PlayStats.Web/L4D2PlayStats/Patents/Configs/PatentConfig.cs
@@ -6,5 +6,5 @@ public static class PatentConfig
     public const int MaxLevel = 15;
 
     public const int MinExperience = 0;
-    public const int MaxExperience = 2_500;
+    public const int MaxExperience = 3_000;
 }
