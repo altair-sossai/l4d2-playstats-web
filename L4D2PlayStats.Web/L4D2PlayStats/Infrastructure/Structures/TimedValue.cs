6e35348bc0448729cea013e8d96a9152b0257625
Altair Sossai <altairsossai@gmail.com>
2024-11-02 14:47:28 -0300

    Refactor TimedValue class and update property logic
    Renamed constructor parameters: `expirationTime` to `expireIn` and `delayTime` to `delay`. Updated default values for `_delay` and `_expireIn`. Refactored `Expired` and `Value` property logic. Added new private method `UpdateValue` to handle value updates.

--- L4D2PlayStats.Web/L4D2PlayStats/Infrastructure/Structures/TimedValue.cs
+++ L4D2PlayStats.Web/L4D2PlayStats/Infrastructure/Structures/TimedValue.cs
@@ -1,43 +1,41 @@
 ﻿namespace L4D2PlayStats.Infrastructure.Structures;
 
-public class TimedValue<T>(T defaultValue = default!, TimeSpan? expirationTime = null, TimeSpan? delayTime = null)
+public class TimedValue<T>(T defaultValue = default!, TimeSpan? expireIn = null, TimeSpan? delay = null)
 {
     private readonly T _defaultValue = defaultValue;
-    private readonly TimeSpan _delayTime = delayTime ?? TimeSpan.FromSeconds(60);
-    private readonly TimeSpan _expirationTime = expirationTime ?? TimeSpan.FromSeconds(60);
+    private readonly TimeSpan _delay = delay ?? TimeSpan.Zero;
+    private readonly TimeSpan _expireIn = expireIn ?? TimeSpan.FromMinutes(1);
 
     private DateTime _lastUpdate = DateTime.MinValue;
     private T _value = defaultValue;
 
-    private bool Expired => DateTime.UtcNow >= _lastUpdate + _expirationTime;
+    private bool Expired => DateTime.UtcNow >= _lastUpdate + _expireIn;
 
     public T Value
     {
         get
         {
-            if (!Expired)
-                return _value;
-
-            _value = _defaultValue;
-            _lastUpdate = DateTime.MinValue;
+            if (Expired)
+                UpdateValue(_defaultValue);
 
             return _value;
         }
         set
         {
-            if (_delayTime == TimeSpan.Zero)
+            if (_delay == TimeSpan.Zero)
             {
-                _value = value;
-                _lastUpdate = DateTime.UtcNow;
+                UpdateValue(value);
                 return;
             }
 
-            Task.Delay(_delayTime).ContinueWith(_ =>
-            {
-                _value = value;
-                _lastUpdate = DateTime.UtcNow;
-            });
+            Task.Delay(_delay).ContinueWith(_ => UpdateValue(value));
         }
+    }
+
+    private void UpdateValue(T value)
+    {
+        _value = value;
+        _lastUpdate = DateTime.UtcNow;
     }
 
     public static implicit operator T(TimedValue<T> timedValue)
