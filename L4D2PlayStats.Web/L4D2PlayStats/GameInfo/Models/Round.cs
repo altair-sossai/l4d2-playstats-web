de93bb21520ca121e4052e8e05f8072d4e29549b
Altair Sossai <altairsossai@gmail.com>
2024-11-09 08:17:49 -0300

    Add TankPercent and WitchPercent properties to Round
    Two new integer properties, TankPercent and WitchPercent, were added to the Round class. These properties likely represent percentages related to the game round, such as the probability or presence of certain elements (e.g., a tank or a witch) within the round.

--- L4D2PlayStats.Web/L4D2PlayStats/GameInfo/Models/Round.cs
+++ L4D2PlayStats.Web/L4D2PlayStats/GameInfo/Models/Round.cs
@@ -4,4 +4,6 @@ public class Round
 {
     public int AreTeamsFlipped { get; set; }
     public int MaxChapterProgressPoints { get; set; }
+    public int TankPercent { get; set; }
+	public int WitchPercent { get; set; }
 }
