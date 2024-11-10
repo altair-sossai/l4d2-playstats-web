d1a76efe6e41182ca65961d85a8e5ab3bd13dcca
Altair Sossai <altairsossai@gmail.com>
2024-11-10 18:46:21 -0300

    Add extension methods and update UI for game info
    Updated `_GameInfo.cshtml` to include new styles and a table for displaying survivor information. Added `MeleeWeaponExtensions` and `WeaponExtensions` classes to provide `WeaponName` methods for respective enums. Enhanced `EnumExtensions` with a `Description` method to retrieve enum descriptions. Minor formatting fix in `Details.cshtml`.

--- L4D2PlayStats.Web/L4D2PlayStats/Infrastructure/Extensions/EnumExtensions.cs
+++ L4D2PlayStats.Web/L4D2PlayStats/Infrastructure/Extensions/EnumExtensions.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel;
+using System.Reflection;
 using L4D2PlayStats.Infrastructure.Attributes;
 
 namespace L4D2PlayStats.Infrastructure.Extensions;
 
 public static class EnumExtensions
 {
+    private static readonly Type DescriptionAttributeType = typeof(DescriptionAttribute);
+
     public static string GetResourceKey(this Enum @enum)
     {
         var type = @enum.GetType();
@@ -17,5 +20,18 @@ public static class EnumExtensions
         var resourceKeyAttribute = fieldInfo.GetCustomAttribute<ResourceKeyAttribute>();
 
         return resourceKeyAttribute != null ? resourceKeyAttribute.Key : value;
+    }
+
+    public static string Description(this Enum @enum)
+    {
+        var type = @enum.GetType();
+        var fieldInfo = type.GetField(@enum.ToString());
+
+        if (fieldInfo?.GetCustomAttributes(DescriptionAttributeType, false) is not DescriptionAttribute[] { Length: > 0 } descriptionAttributes)
+            return @enum.ToString();
+
+        var descriptionAttribute = descriptionAttributes.First();
+
+        return descriptionAttribute.Description;
     }
 }
