fcd7d1cc573ade28e9cf8efc6f3418c4152264fd
Altair Sossai <altairsossai@gmail.com>
2024-11-12 08:19:01 -0300

    Add HasIcon methods and update _Players.cshtml
    Added HasIcon extension methods for MeleeWeapon and Weapon enums in MeleeWeaponExtensions.cs and WeaponExtensions.cs. Updated _Players.cshtml to use these methods for filtering weapons with icons before rendering. Included L4D2PlayStats.Web.Extensions namespace in _Players.cshtml.

---
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Extensions/MeleeWeaponExtensions.cs
@@ -0,0 +1,24 @@
+﻿using L4D2PlayStats.GameInfo.Enums;
+
+namespace L4D2PlayStats.Web.Extensions;
+
+public static class MeleeWeaponExtensions
+{
+    public static bool HasIcon(this MeleeWeapon? meleeWeapon)
+    {
+        return meleeWeapon is MeleeWeapon.Knife
+            or MeleeWeapon.BaseballBat
+            or MeleeWeapon.MeleeChainsaw
+            or MeleeWeapon.CricketBat
+            or MeleeWeapon.Crowbar
+            or MeleeWeapon.ElectricGuitar
+            or MeleeWeapon.Fireaxe
+            or MeleeWeapon.FryingPan
+            or MeleeWeapon.GolfClub
+            or MeleeWeapon.Katana
+            or MeleeWeapon.Machete
+            or MeleeWeapon.Tonfa
+            or MeleeWeapon.Shovel
+            or MeleeWeapon.Pitchfork;
+    }
+}
---
+++ L4D2PlayStats.Web/L4D2PlayStats.Web/Extensions/WeaponExtensions.cs
@@ -0,0 +1,41 @@
+﻿using L4D2PlayStats.GameInfo.Enums;
+
+namespace L4D2PlayStats.Web.Extensions;
+
+public static class WeaponExtensions
+{
+    public static bool HasIcon(this Weapon? weapon)
+    {
+        return weapon is Weapon.Pistol
+            or Weapon.Smg
+            or Weapon.Pumpshotgun
+            or Weapon.Autoshotgun
+            or Weapon.Rifle
+            or Weapon.HuntingRifle
+            or Weapon.SmgSilenced
+            or Weapon.ShotgunChrome
+            or Weapon.RifleDesert
+            or Weapon.SniperMilitary
+            or Weapon.ShotgunSpas
+            or Weapon.FirstAidKit
+            or Weapon.Molotov
+            or Weapon.PipeBomb
+            or Weapon.PainPills
+            or Weapon.Melee
+            or Weapon.Chainsaw
+            or Weapon.GrenadeLauncher
+            or Weapon.Adrenaline
+            or Weapon.Defibrillator
+            or Weapon.Vomitjar
+            or Weapon.RifleAk47
+            or Weapon.ColaBottles
+            or Weapon.IncendiaryAmmo
+            or Weapon.FragAmmo
+            or Weapon.PistolMagnum
+            or Weapon.SmgMp5
+            or Weapon.RifleSg552
+            or Weapon.SniperAwp
+            or Weapon.SniperScout
+            or Weapon.RifleM60;
+    }
+}
