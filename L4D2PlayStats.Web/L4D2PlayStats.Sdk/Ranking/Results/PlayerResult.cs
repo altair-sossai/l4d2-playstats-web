59759cac2bd70ec5486be64bde40ddf22099f5bc
Altair Sossai <altairsossai@gmail.com>
2024-12-03 15:36:50 -0300

    Revert "Add player comparison feature and update localization"
    This reverts commit 8fc1e488e6b67f7fa36af389c453bd9e31c53661.

--- L4D2PlayStats.Web/L4D2PlayStats.Sdk/Ranking/Results/PlayerResult.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Sdk/Ranking/Results/PlayerResult.cs
@@ -52,6 +52,4 @@ public class PlayerResult
     public int MvpSiDamage { get; set; }
     public int MvpCommon { get; set; }
     public int LvpFfGiven { get; set; }
-
-    public List<AnotherPlayerResult>? Anothers { get; set; }
 }
