df280d4fecccfe4e3a1610c89bf650e21232210d
Altair Sossai <altairsossai@gmail.com>
2025-05-07 08:55:13 -0300

    Improve timeout handling in PlayerService
    - Introduced a `timeout` constant for configuring send and receive timeouts in `UdpClient`, enhancing flexibility.
    - Updated `SendTimeout` and `ReceiveTimeout` to use the new constant instead of hardcoded values.
    - Refactored `GetPlayersAsync` to utilize `Task.WhenAny` for better asynchronous receive operation handling, allowing for timeout control.
    - Enhanced error handling by awaiting the result of the receive operation after checking for timeout completion.

--- L4D2PlayStats.Web/L4D2PlayStats/Steam/Players/Services/PlayerService.cs
+++ L4D2PlayStats.Web/L4D2PlayStats/Steam/Players/Services/PlayerService.cs
@@ -10,16 +10,22 @@ public class PlayerService : IPlayerService
 
     public async IAsyncEnumerable<Player> GetPlayersAsync(string ip, int port)
     {
+        const int timeout = 2000;
+
         var ipAddress = IPAddress.Parse(ip);
         var ipEndPoint = new IPEndPoint(ipAddress, port);
 
         using var udpClient = new UdpClient();
-        udpClient.Client.SendTimeout = 5000;
-        udpClient.Client.ReceiveTimeout = 5000;
+        udpClient.Client.SendTimeout = timeout;
+        udpClient.Client.ReceiveTimeout = timeout;
 
         await udpClient.SendAsync(Request, Request.Length, ipEndPoint);
 
-        var result = await udpClient.ReceiveAsync();
+        var receiveTask = udpClient.ReceiveAsync();
+        if (await Task.WhenAny(receiveTask, Task.Delay(timeout)) != receiveTask)
+            yield break;
+
+        var result = receiveTask.Result;
         var bytes = result.Buffer;
         if (bytes.Length != 9 || bytes[4] != 0x41)
             yield break;
@@ -27,7 +33,11 @@ public class PlayerService : IPlayerService
         bytes[4] = 0x55;
         await udpClient.SendAsync(bytes, bytes.Length, ipEndPoint);
 
-        result = await udpClient.ReceiveAsync();
+        receiveTask = udpClient.ReceiveAsync();
+        if (await Task.WhenAny(receiveTask, Task.Delay(timeout)) != receiveTask)
+            yield break;
+
+        result = receiveTask.Result;
 
         using var memoryStream = new MemoryStream(result.Buffer);
         var binaryReader = new BinaryReader(memoryStream, Encoding.UTF8);
