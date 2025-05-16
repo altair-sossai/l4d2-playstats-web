1118e8dcdaa444a18c3ef85979c848a6f4154eaf
Altair Sossai <altairsossai@gmail.com>
2025-05-16 14:55:47 -0300

    Refactor resource keys for clarity and consistency
    Updated `PlayerResultProperty` enum and resource files to replace `FF` with `FfGiven` and `FfDamageTotal`. Renamed `TanksKilled` to `TankKilled` and `WitchesKilled` to `WitchKilled`. Other resource keys related to round timing and tank events have been standardized. Updated `Details.cshtml` and `Statistics.cshtml` to reflect these changes, enhancing overall clarity in the codebase.

--- L4D2PlayStats.Web/L4D2PlayStats.Core/Players/Enums/PlayerResultProperty.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Core/Players/Enums/PlayerResultProperty.cs
@@ -61,7 +61,7 @@ public enum PlayerResultProperty
     [ResourceKey("Levels")]
     Levels,
 
-    [ResourceKey("FF")]
+    [ResourceKey("FfGiven")]
     FfGiven,
 
     [ResourceKey("TotalDamage")]
