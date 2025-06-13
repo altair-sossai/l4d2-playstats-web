87ceaac036fef4225ee91544d0547d4e9f97f616
Altair Sossai <altairsossai@gmail.com>
2025-06-13 11:15:27 -0300

    Add TruncatePlayerName method and update CSS styles
    Introduced a new `TruncatePlayerName` method in the `StringExtensions` class to improve player name truncation logic. This method is utilized across multiple Razor views for better user experience.
    
    Updated the `_Layout.cshtml` CSS file to include a `.line-break-anywhere` class for enhanced layout responsiveness.

--- L4D2PlayStats.Web/L4D2PlayStats.Core/Infrastructure/Extensions/StringExtensions.cs
+++ L4D2PlayStats.Web/L4D2PlayStats.Core/Infrastructure/Extensions/StringExtensions.cs
@@ -4,6 +4,17 @@ namespace L4D2PlayStats.Core.Infrastructure.Extensions;
 
 public static class StringExtensions
 {
+    public static string TruncatePlayerName(this string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            return name;
+
+        if (name.Length >= 15 && !name.Contains(' '))
+            return name.Truncate(10).Trim();
+
+        return name.Truncate(30).Trim();
+    }
+
     public static string Truncate(this string value, int max)
     {
         return value.Length <= max ? value : value[..max];
