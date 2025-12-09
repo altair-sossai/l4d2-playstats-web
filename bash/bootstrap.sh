14d99c78c0793e9400ed5773457ee6aea0fd3656
Altair Sossai <altairsossai@gmail.com>
2025-12-08 21:13:28 -0300

    Set system timezone to UTC in bootstrap.sh
    Changed timedatectl configuration in bootstrap.sh to set the system timezone to UTC instead of America/Sao_Paulo for consistency across deployments. No other modifications were made.

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo timedatectl set-timezone America/Sao_Paulo
+sudo timedatectl set-timezone UTC
 
 sudo chmod +x /home/l4d2-playstats-web/L4D2PlayStats.Web
 sudo screen -d -m -S "l4d2-playstats-web" sh -c 'cd /home/l4d2-playstats-web && ./L4D2PlayStats.Web'
